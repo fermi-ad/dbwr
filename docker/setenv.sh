1aa53b247cfb33d0c367db249db7b2ee2a75f21c
Maria A. <mapsacosta@gmail.com>
2025-07-22 14:54:26 -0500

    Another whitelist for testing

--- docker/setenv.sh
+++ docker/setenv.sh
@@ -1,4 +1,4 @@
 export WHITELIST1=file:/displays/CSS/.*
 export WHITELIST2=http://vclx4.fnal.gov/.*
 export WHITELIST3=https://dbwr-httpd.fnal.gov/displays/.*
-
+export WHITELIST4=https://dbwr-test.fnal.gov/displays/.*
